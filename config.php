36bbb47829f61d9d0282ea10dfa6a3fda7533ae0
Charles Ma <charles.marion@kitware.com>
2011-08-29 12:15:59 +0200

    BUG: fixed php bug

--- config.php
+++ config.php
@@ -6,8 +6,4 @@ $FOLDERS="Algorithms;BasicFilters;Common;IO;Numerics;Review;SpatialObject";
 $TMP_PATH="/home/charles/ITKTMP";
 $GIT_EXECUTABLE="git";
 $GIT_PATH_MAIN="/home/charles/ITK";
-$GIT_PATH_SUB1="/home/charles/ITKSUB1";
-$GIT_PATH_SUB2="/home/charles/ITKSUB2";
 ?>
-
-
--- js/editdoxygen.js
+++ js/editdoxygen.js
@@ -2,6 +2,6 @@ jQuery(function($)
 {
 if($("div.title").html().indexOf("Class Template Reference")!=-1)
   {
-  $("div.summary").prepend('<a href="http://www.itk.org/editdoc/editcomments.php?file='+$("code a.el:first").html()+'">Edit comments</a> |');
+  $("div.summary").prepend('<a href="http://ij.itk.org/itkcomment/editcomments.php?file='+$("code a.el:first").html()+'">Edit comments</a> |');
   }
 });
